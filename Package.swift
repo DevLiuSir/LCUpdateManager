a97960d6286b1b312c2b8f50b8ca146d275365f9
Marvin <93428739@qq.com>
2024-12-11 00:19:30 +0800

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+
+let package = Package(
+    name: "LCUpdateManager",
+    defaultLocalization: "en", // 指定默认语言为英文
+    platforms: [
+        .macOS(.v10_15) // 指定支持的 macOS 版本
+    ],
+    products: [
+        .library(
+            name: "LCUpdateManager",
+            targets: ["LCUpdateManager"]),
+    ],
+    targets: [
+        .target(
+            name: "LCUpdateManager",
+            resources: [
+                .process("Resources") // 添加资源路径
+            ]),
+        .testTarget(
+            name: "LCUpdateManagerTests",
+            dependencies: ["LCUpdateManager"]),
+    ]
+)
